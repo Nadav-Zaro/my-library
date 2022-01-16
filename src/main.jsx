19b46706b45ee460be97dab2626a50e4e14792ff
Nzaro22 <nzaro4@gmail.com>
2022-01-16 18:41:10 +0200

    heroku try

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import * as style from './components/style.module.css'
 import App from './App'
 
 ReactDOM.render(
